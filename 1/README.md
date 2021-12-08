beaa551c5d16575f68ccf4a027d51883714863e7
Andrew Nelson <andrew2nelson@arista.com>
2021-12-07 22:21:41 -0800

    Day 1b - Apply boxcar filter before checking for increases

--- 1/README.md
+++ 1/README.md
@@ -1,37 +1,5 @@
 # Day 1a: Sonar Sweep
 
 Given a stdin that has a sequence of ints (one int per line),
+apply a window=3 boxcar filter and
 count how many subsequent pairs of lines are increasing.
-
-Example input
-```
-199
-200
-208
-210
-200
-207
-240
-269
-260
-263
-```
-
-Annotated input
-```
-199 (N/A - no previous measurement)
-200 (increased)
-208 (increased)
-210 (increased)
-200 (decreased)
-207 (increased)
-240 (increased)
-269 (increased)
-260 (decreased)
-263 (increased)
-```
-
-Output
-```
-7
-```
--- 1/main.rs
+++ 1/main.rs
@@ -6,9 +6,18 @@ fn input_ints() -> Vec<i32> {
 	).collect()
 }
 
+// Sum inputs in windows of 3.
+fn boxcar_sum(signal : Vec<i32>) -> Vec<i32> {
+    let mut result = Vec::new();
+    for n in 0..(signal.len() - 2) {
+        result.push(signal[n] + signal[n+1] + signal[n+2])
+    }
+    result
+}
+
 // Count how many subsequent inputs are increasing.
 fn count_increasing_depths() -> i32 {
-	let depths = input_ints();
+	let depths = boxcar_sum(input_ints());
 	let mut prev = depths[0];
 	let mut increases_count = 0;
 	for depth in depths.iter() {
